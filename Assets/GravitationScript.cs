a64c34e905a11f5c728ba7f3b67765c5fefb7c10
Unknown <shigure@hotmail.de>
2018-06-26 17:04:25 +0200

    First Commit
    -Added Gravitational Sphere
    -Added Ions
    -Added Constraints

---
+++ Assets/GravitationScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GravitationScript : MonoBehaviour {
+
+    public float moveSpeed = 2.0f;
+    public Camera mainCam;
+
+
+    void OnTriggerStay(Collider col)
+    {
+        
+        if (col.GetComponent<Collider>().tag == "Ion")
+        {
+            Vector3 targetPos = this.transform.position - col.transform.position;
+            //targetPos.z = transform.position.z;
+            Debug.Log(targetPos);
+            //col.transform.Translate(targetPos * (moveSpeed * Time.deltaTime));
+            col.GetComponent<Rigidbody>().AddForce(targetPos);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = cursorWorldPos();
+    }
+
+    public Vector3 cursorWorldPos()
+    {
+        return new Vector3(mainCam.ScreenToWorldPoint(Input.mousePosition).x, mainCam.ScreenToWorldPoint(Input.mousePosition).y, 0.0f);
+    }
+	
+
+}
