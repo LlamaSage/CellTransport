fbfa153dbbb8a867b9ee5412734e17bc982ad33c
Unknown <shigure@hotmail.de>
2018-08-15 21:57:14 +0200

    Wisdom Tooth Extraction Update
    -Added barriers to the middle canal
    -Added a locking mechanism, currently only from top to bottom, for the transfer
    -Added several colliders for the locking mechanism
    -Increased sizes of ions in general, as well as adjust the membrane scale

---
+++ Assets/NaCaTunnelScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaCaTunnelScript : MonoBehaviour {
+
+    public NatLockScript[] NaScript;
+    public GameObject lowerWall;
+    public GameObject upperWall;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool isFull = true;
+		foreach (NatLockScript na in NaScript)
+        {
+            isFull = na.locked;
+            if (!isFull)
+            {
+                return;
+            }
+        }
+        lowerWall.SetActive(false);
+        upperWall.SetActive(true);
+        foreach (NatLockScript na in NaScript)
+            na.Expell(new Vector3(0.0f, -110.0f, 0.0f));
+
+    }
+}
---
+++ Assets/NatLockScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NatLockScript : MonoBehaviour {
+
+    public bool locked = false;
+    public float cooldown = 5;
+    private float localCooldown = 0.0f;
+    private GameObject occupant;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(!locked && localCooldown<=0.0f && other.gameObject.tag == "Natrium")
+        {
+            other.gameObject.transform.SetPositionAndRotation(this.GetComponent<SphereCollider>().transform.position, Quaternion.Euler(new Vector3(90.0f, 0.0f, 0.0f)));
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            occupant = other.gameObject;
+            this.locked = true;
+        }
+
+    }
+
+    public void Update()
+    {
+        if (localCooldown > 0.0f)
+            this.localCooldown -= Time.deltaTime;
+    }
+
+    public void Expell(Vector3 direction)
+    {
+        localCooldown = cooldown;
+        occupant.GetComponent<Rigidbody>().isKinematic = false;
+        occupant.GetComponent<Rigidbody>().AddForce(direction);
+        occupant = null;
+        locked = false;
+
+    }
+}
